949cadc890e8f6455180c893dc98bab33e1e933c
Lancaster07 <eulerlancaster22@gmail.com>
2024-06-29 22:17:41 -0300

    Projet iPhone

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor){
+    let circle = document.querySelector(".circulo")
+    circle.style.background = cor
+}
+
+function trocaImagem(imagem){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
